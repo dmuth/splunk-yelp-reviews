189b6afd388674b7bee0f110dd1618680a47ef13
Douglas Muth <dmuth@dmuth.org>
2019-04-23 20:57:14 -0400

    Renamed urlst.txt and script to download them.

--- bin/download-reviews.sh
+++ bin/download-reviews.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Download reviews from all of our restaurants
+# Download a review
 #
 
 # Errors are fatal
@@ -9,32 +9,29 @@ set -e
 if test "$1" == "" -o "$1" == "-h" -o "$1" == "--help"
 then
 	echo "! "
-	echo "! Symtax: $0 file"
+	echo "! Symtax: $0 url"
 	echo "! "
-	echo "! file - Text file of Yelp venue URLs to grab comments from."
+	echo "! url - URL of a venue on Yelp"
 	echo "! "
 	exit 1
 fi
 
-URLS=$1
+URL=$1
 
 THIS_DIR=$(dirname $0)
 
+TARGET=logs/$(echo $URL | sed -e "s/.*\///").json
 
-for URL in $(cat $URLS)
-do
-	TARGET=logs/$(echo $URL | sed -e "s/.*\///").json
-	if test -f $TARGET
-	then
-		echo "# Target '${TARGET}' exists, skipping..."
-		continue
-	fi
+if test -f $TARGET
+then
+	echo "# Target '${TARGET}' exists, skipping..."
 
+else
 	TMP=$(mktemp)
 	$THIS_DIR/download-reviews.py $URL > $TMP
 	mv $TMP $TARGET
 
-done
+fi
 
 
 
